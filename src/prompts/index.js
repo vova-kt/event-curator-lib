68e99d2990a2829b8df76f043ae744e00b9a52f4
Vladimir.Muzhichenko <vladimir.muzhichenko@jetbrains.com>
2026-05-04 18:34:45 +0200

    remove dedup

--- src/prompts/index.js
+++ src/prompts/index.js
@@ -1,5 +1,4 @@
 export { extractEventsPrompt, extractEventsSchema } from './extractEvents.js';
-export { dedupeJudgePrompt, dedupeJudgeSchema } from './dedupeJudge.js';
 export { rankByPreferencePrompt, rankByPreferenceSchema } from './rankByPreference.js';
 export { derivePreferenceTraitsPrompt, derivePreferenceTraitsSchema } from './derivePreferenceTraits.js';
 export { expandQueriesPrompt, expandQueriesSchema } from './expandQueries.js';
